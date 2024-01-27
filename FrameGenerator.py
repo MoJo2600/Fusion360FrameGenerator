1054c19a0ac58bb027290dfc17d147c4d72e4bf9
Christian Erhardt <christian.erhardt@mojo2k.de>
2024-01-27 16:44:15 +0100

    feat: Rods are now generated and length is shown
    The rods do now show the length
    
    Signed-off-by: Christian Erhardt <christian.erhardt@mojo2k.de>

--- FrameGenerator.py
+++ FrameGenerator.py
@@ -3,52 +3,88 @@
 
 import adsk.core, adsk.fusion, adsk.cam, traceback, os
 
+defaultRodDiameter = .3
+defaultConnectorLength = 2.5
+defaultWallThickness = .2
+defaultClearance = .015
+
+def createNewComponent(name: str):
+    # Get the active design.
+    product = app.activeProduct
+    design = adsk.fusion.Design.cast(product)
+    rootComp = design.rootComponent
+    allOccs = rootComp.occurrences
+    newOcc = allOccs.addNewComponent(adsk.core.Matrix3D.create())
+    newOcc.component.name = name
+    return newOcc.component
+
 # global set of event handlers to keep them referenced for the duration of the command
 handlers = []
 app = adsk.core.Application.get()
 if app:
+
+    # Disable history otherwise temp brepbody is not working
+    des = adsk.fusion.Design.cast(app.activeProduct)
+    des.designType = adsk.fusion.DesignTypes.DirectDesignType  
+
     ui = app.userInterface
 
     doc = app.activeDocument
     d = doc.design
+    
     rootComp: adsk.fusion.Component = d.rootComponent
     tempBrepMgr = adsk.fusion.TemporaryBRepManager.get()  
-    bodies = rootComp.bRepBodies
 
-# TODO: detect if history is on and guide user to disable it
+    connectorComp = createNewComponent("Connectors")
+    bodies = connectorComp.bRepBodies
 
-class IntersectionValidateInputHandler(adsk.core.ValidateInputsEventHandler):
-    def __init__(self):
-        super().__init__()
+    rodComp = createNewComponent("Connection Rods")
+    rodBodies = rodComp.bRepBodies
+    
+
+# class FrameGeneratorValidateInputHandler(adsk.core.ValidateInputsEventHandler):
+#     def __init__(self):
+#         super().__init__()
        
-    def notify(self, args):
-        try:
-            sels = ui.activeSelections
-            if len(sels) == 1:
-                args.areInputsValid = True
-        except:
-            if ui:
-                ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
+#     def notify(self, args):
+#         try:
+#             sels = ui.activeSelections
+#             if len(sels) == 1:
+#                 args.areInputsValid = True
+#         except:
+#             if ui:
+#                 ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
 
-class IntersectionCommandExecuteHandler(adsk.core.CommandEventHandler):
+class FrameGeneratorCommandExecuteHandler(adsk.core.CommandEventHandler):
     def __init__(self):
         super().__init__()
     def notify(self, args):
         try:
+            unitsMgr = app.activeProduct.unitsManager
             command = args.firingEvent.sender
             inputs = command.commandInputs
 
-            input0 = inputs[0]
-            sel0 = input0.selection(0)
-
             frameGenerator = FrameGenerator()
-            frameGenerator.Execute(sel0.entity)
+            for input in inputs:
+                if input.id == 'entity':
+                    selection = input.selection(0)
+                    frameGenerator.entity = selection.entity
+                elif input.id == 'rodDiameter':
+                    frameGenerator.rodDiameter = unitsMgr.evaluateExpression(input.expression, "cm")
+                elif input.id == 'connectorLength':
+                    frameGenerator.connectorLength = unitsMgr.evaluateExpression(input.expression, "cm")
+                elif input.id == 'wallThickness':
+                    frameGenerator.wallThickness = unitsMgr.evaluateExpression(input.expression, "mm")
+                elif input.id == 'clearance':
+                    frameGenerator.clearance = unitsMgr.evaluateExpression(input.expression, "mm")
+
+            frameGenerator.Execute()
         except:
             if ui:
                 ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
 
 
-class IntersectionCommandDestroyHandler(adsk.core.CommandEventHandler):
+class FrameGeneratorCommandDestroyHandler(adsk.core.CommandEventHandler):
     def __init__(self):
         super().__init__()
     def notify(self, args):
@@ -60,30 +96,39 @@ class IntersectionCommandDestroyHandler(adsk.core.CommandEventHandler):
             if ui:
                 ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
 
-class IntersectionCommandCreatedHandler(adsk.core.CommandCreatedEventHandler):
+class FrameGeneratorCommandCreatedHandler(adsk.core.CommandCreatedEventHandler):
     def __init__(self):
         super().__init__()
     def notify(self, args):
         try:
             cmd = args.command
-            onExecute = IntersectionCommandExecuteHandler()
+            onExecute = FrameGeneratorCommandExecuteHandler()
             cmd.execute.add(onExecute)
-            onDestroy = IntersectionCommandDestroyHandler()
+            onDestroy = FrameGeneratorCommandDestroyHandler()
             cmd.destroy.add(onDestroy)
 
-            onValidateInput = IntersectionValidateInputHandler()
-            cmd.validateInputs.add(onValidateInput)
+            # onValidateInput = FrameGeneratorValidateInputHandler()
+            # cmd.validateInputs.add(onValidateInput)
             # keep the handler referenced beyond this function
             handlers.append(onExecute)
             handlers.append(onDestroy)
-            handlers.append(onValidateInput)
+            # handlers.append(onValidateInput)
             #define the inputs
             inputs = cmd.commandInputs
             i1 = inputs.addSelectionInput('entity', 'Entity One', 'Please select a BRepBody')
-
             i1.addSelectionFilter(adsk.core.SelectionCommandInput.MeshBodies)
-            # i1.addSelectionFilter(adsk.core.SelectionCommandInput.Occurrences)
-            # i1.addSelectionFilter(adsk.core.SelectionCommandInput.RootComponents)
+
+            initRodDiameter = adsk.core.ValueInput.createByReal(defaultRodDiameter)
+            inputs.addValueInput('rodDiameter', 'Connection Rod Diameter', 'mm', initRodDiameter)
+
+            initConnectorLength = adsk.core.ValueInput.createByReal(defaultConnectorLength)
+            inputs.addValueInput('connectorLength', 'Connector Length', 'mm', initConnectorLength)
+
+            initWallThickness = adsk.core.ValueInput.createByReal(defaultWallThickness)
+            inputs.addValueInput('wallThickness', 'Connector Wall Thickness', 'mm', initWallThickness)
+
+            initClearance = adsk.core.ValueInput.createByReal(defaultClearance)
+            inputs.addValueInput('clearance', 'Print Clearance', 'mm', initClearance)
 
         except:
             if ui:
@@ -92,37 +137,96 @@ class IntersectionCommandCreatedHandler(adsk.core.CommandCreatedEventHandler):
 class FrameGenerator:
     # UNIT: cm
     # TODO: configurable
-    rod_diameter = .4
 
-    cylinder_length = 2.5
-    wall_thickness = .1
-    cylinder_radius = rod_diameter / 2 + wall_thickness
-    sphere_radius = cylinder_radius * 1.5
+    _entity: adsk.fusion.MeshBody
+    _visitedConnectors = {}
+    _visitedRods = {}
+    _nodeBodies = {}
 
-    entity: adsk.fusion.MeshBody
-    visited_cylinders = {}
-    node_bodies = {}
+    def __init__(self):
+        self._rodDiameter = defaultRodDiameter
+        self._wallThickness = defaultWallThickness
+        self._connectorLength = adsk.core.ValueInput.createByReal(defaultConnectorLength)
+        self._rodNumber = 1
 
-    def add_cylinder(self, node_a_idx, node_b_idx):
+    #properties      
+    @property
+    def entity(self):
+        return self._entity
+    @entity.setter
+    def entity(self, value: adsk.fusion.MeshBody):
+        self._entity = value
+
+    @property
+    def rodDiameter(self):
+        return self._rodDiameter
+    @rodDiameter.setter
+    def rodDiameter(self, value):
+        self._rodDiameter = value
+
+    @property
+    def wallThickness(self):
+        return self._wallThickness
+    @wallThickness.setter
+    def wallThickness(self, value):
+        self._wallThickness = value
+
+    @property
+    def connectorLength(self):
+        return self._connectorLength
+    @connectorLength.setter
+    def connectorLength(self, value):
+        self._connectorLength = value
+
+    @property
+    def clearance(self):
+        return self._clearance
+    @clearance.setter
+    def clearance(self, value):
+        self._clearance = value
+
+    @property
+    def connectorRadius(self):
+        return self._rodDiameter / 2 + self._wallThickness / 2
+
+    @property
+    def sphereRadius(self):
+        return self.connectorRadius * 1.5
+
+    def create_connector(self, node_a_idx, node_b_idx):
+        """ Creates one connector with a hole
+        :type self:
+        :param self:
+    
+        :type node_a_idx:
+        :param node_a_idx:
+    
+        :type node_b_idx:
+        :param node_b_idx:
+    
+        :raises:
+    
+        :rtype:
+        """
         # check if the cylinder was already created
-        cylinder_key = (node_a_idx, node_b_idx)
+        connectorKey = (node_a_idx, node_b_idx)
 
-        if cylinder_key in self.visited_cylinders:
+        if connectorKey in self._visitedConnectors:
             return
 
-        self.visited_cylinders[cylinder_key] = True
+        self._visitedConnectors[connectorKey] = True
 
-        node_a = self.entity.mesh.nodeCoordinates[node_a_idx]
-        node_b = self.entity.mesh.nodeCoordinates[node_b_idx]
+        node_a = self._entity.mesh.nodeCoordinates[node_a_idx]
+        node_b = self._entity.mesh.nodeCoordinates[node_b_idx]
 
         # create cylinder (shell)
         start_vector_a_b = node_a.vectorTo(node_b)
         start_vector_a_b.normalize()
-        start_vector_a_b.scaleBy(self.sphere_radius * 0.75)
+        start_vector_a_b.scaleBy(self.sphereRadius * 0.75)
 
         end_vector_a_b = node_a.vectorTo(node_b)
         end_vector_a_b.normalize()
-        end_vector_a_b.scaleBy(self.cylinder_length)
+        end_vector_a_b.scaleBy(self._connectorLength)
 
         start_point_a_b = node_a.copy()
         start_point_a_b.translateBy(start_vector_a_b)
@@ -130,15 +234,15 @@ class FrameGenerator:
         target_point_a_b = node_a.copy()
         target_point_a_b.translateBy(end_vector_a_b)
         
-        cylinder = tempBrepMgr.createCylinderOrCone(start_point_a_b, self.cylinder_radius, target_point_a_b, self.cylinder_radius)
+        cylinder = tempBrepMgr.createCylinderOrCone(start_point_a_b, self.connectorRadius, target_point_a_b, self.connectorRadius)
         shell_cylinder = bodies.add(cylinder)
         
         # create cylinder to cut out
-        cylinder_tool_body = tempBrepMgr.createCylinderOrCone(start_point_a_b, self.cylinder_radius-self.wall_thickness, target_point_a_b, self.cylinder_radius-self.wall_thickness)
+        cylinder_tool_body = tempBrepMgr.createCylinderOrCone(start_point_a_b, self.connectorRadius-self._wallThickness/2+self._clearance/2, target_point_a_b, self.connectorRadius-self._wallThickness/2+self._clearance/2)
         tool_body = bodies.add(cylinder_tool_body)
 
         # cut hole
-        shell_cut = rootComp.features.combineFeatures
+        shell_cut = connectorComp.features.combineFeatures
         cut_tools = adsk.core.ObjectCollection.create()
         cut_tools.add(tool_body)
         cut_input: adsk.fusion.CombineFeatureInput = shell_cut.createInput(shell_cylinder, cut_tools)
@@ -148,51 +252,95 @@ class FrameGenerator:
         shell_cut.add(cut_input)
 
         # join with other bodies of node (sphere + n cylinders)
-        node_combine = rootComp.features.combineFeatures
+        node_combine = connectorComp.features.combineFeatures
         join_tools = adsk.core.ObjectCollection.create()
         join_tools.add(shell_cylinder)
-        join_input: adsk.fusion.CombineFeatureInput = node_combine.createInput(self.node_bodies[node_a_idx], join_tools)
+        join_input: adsk.fusion.CombineFeatureInput = node_combine.createInput(self._nodeBodies[node_a_idx], join_tools)
         join_input.isNewComponent = False
         join_input.isKeepToolBodies = False
         join_input.operation = adsk.fusion.FeatureOperations.JoinFeatureOperation
         node_combine.add(join_input)
 
+
+    def create_rod(self, node_a_idx: int, node_b_idx: int):
+        # check if the cylinder was already created
+        rodKey = (node_a_idx, node_b_idx)
+
+        if (node_a_idx, node_b_idx) in self._visitedRods or \
+           (node_b_idx, node_a_idx) in self._visitedRods:
+            return
+
+        self._visitedRods[rodKey] = True
+
+        node_a = self._entity.mesh.nodeCoordinates[node_a_idx]
+        node_b = self._entity.mesh.nodeCoordinates[node_b_idx]
+
+        start_vector_a_b = node_a.vectorTo(node_b)
+        start_vector_a_b.normalize()
+        start_vector_a_b.scaleBy(self.sphereRadius * 1.1)  # TODO: clearance
+
+        start_point_a_b = node_a.copy()
+        start_point_a_b.translateBy(start_vector_a_b)
+
+
+
+        end_vector_a_b = node_a.vectorTo(node_b)
+        # end_vector_a_b.normalize()
+        # end_vector_a_b.scaleBy(self.sphereRadius * 0.75)  # TODO: clearance
+        end_vector_a_b.subtract(start_vector_a_b)
+
+        end_point_a_b = node_a.copy()
+        end_point_a_b.translateBy(end_vector_a_b)
+
+        # end_vector_a_b = node_a.vectorTo(node_b)
+        # end_vector_a_b.normalize()
+        # end_vector_a_b.scaleBy(self.sphereRadius * 0.75) # TODO: clearance
+        
+        # end = start_point_a_b.copy()
+
+        # vec = node_b.vectorTo(start_point_a_b)
+
+
+        # vec.subtract(start_vector_a_b)
+
+        # target_point_a_b = node_a.copy()
+        # target_point_a_b.translateBy(vec)
+
+        rodLength = start_point_a_b.distanceTo(end_point_a_b)
+
         # create rod
-        # TODO: shorten rod length to match cutout
-        # TODO: Naming / move rods to own component
-        # TODO: move to separate component
-        rod = tempBrepMgr.createCylinderOrCone(node_a, self.cylinder_radius-self.wall_thickness, node_b, self.cylinder_radius-self.wall_thickness)
-        bodies.add(rod)
+        rod = tempBrepMgr.createCylinderOrCone(start_point_a_b, self.rodDiameter/2, end_point_a_b, self.rodDiameter/2)
+        addedBody = rodBodies.add(rod)
+        addedBody.name = f"Rod {self._rodNumber} - {rodLength:.1f} cm"
+        self._rodNumber = self._rodNumber + 1
 
-
-    def Execute(self, entity: adsk.fusion.MeshBody):
-
-        self.entity = entity
+    def Execute(self):
         # https://help.autodesk.com/view/fusion360/ENU/?guid=GUID-0f6e9ca0-dc67-49c3-b902-baf881063e24
 
-        for node_idx, node in enumerate(entity.mesh.nodeCoordinates):
-            sphere = tempBrepMgr.createSphere(node, self.sphere_radius)
+        for node_idx, node in enumerate(self._entity.mesh.nodeCoordinates):
+            sphere = tempBrepMgr.createSphere(node, self.sphereRadius)
             node_body = bodies.add(sphere)
-            self.node_bodies[node_idx] = node_body
+            self._nodeBodies[node_idx] = node_body
 
-
-        triangles = [entity.mesh.triangleNodeIndices[i*3:i*3+3] for i in range(0, entity.mesh.triangleCount)]
-
-        # A -- B
-        #  \  /
-        #   C
+        triangles = [self._entity.mesh.triangleNodeIndices[i*3:i*3+3] for i in range(0, self._entity.mesh.triangleCount)]
 
         for triangle in triangles:
-            self.add_cylinder(triangle[0], triangle[1])
-            self.add_cylinder(triangle[0], triangle[2])
-            self.add_cylinder(triangle[1], triangle[2])
-           
-            self.add_cylinder(triangle[1], triangle[0])
-            self.add_cylinder(triangle[2], triangle[0])
-            self.add_cylinder(triangle[2], triangle[1])
+            # one direction
+            self.create_connector(triangle[0], triangle[1])
+            self.create_connector(triangle[0], triangle[2])
+            self.create_connector(triangle[1], triangle[2])
 
+            # the other direction
+            self.create_connector(triangle[1], triangle[0])
+            self.create_connector(triangle[2], triangle[0])
+            self.create_connector(triangle[2], triangle[1])
 
-        return self.entity
+            # create rods for the triangle
+            self.create_rod(triangle[0], triangle[1])
+            self.create_rod(triangle[0], triangle[2])
+            self.create_rod(triangle[1], triangle[2])
+
+        return self._entity
 
 def run(context):
     try:
@@ -210,7 +358,7 @@ def run(context):
                     'Generate a frame for a body',
                     resourceDir)
 
-        onCommandCreated = IntersectionCommandCreatedHandler()
+        onCommandCreated = FrameGeneratorCommandCreatedHandler()
         cmdDef.commandCreated.add(onCommandCreated)
         # keep the handler referenced beyond this function
         handlers.append(onCommandCreated)
